43bee8508ff782419eb82ebd7b56f71c44a9098d
khushboodhiman <khushboodhiman716@gmail.com>
2022-04-17 19:07:18 +0530

    Q1 - Added basic routes

---
+++ index.js
@@ -0,0 +1,24 @@
+// Calling Express to create a base object from it i.e. app
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Used because without this node doesn't executes static files like css and pngs
+app.use(express.static('public'));
+
+// Route with handler which will be executed when route matches the given route in the statement i.e. / or home
+app.get('/', function(req, res){
+
+    // sendFile is used to send files when requested and not only text.
+   res.sendFile(path.join(__dirname, '/public/home.html'));
+});
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, '/public/about.html'));
+ });
+
+app.get('/contact', function(req, res){
+    res.sendFile(path.join(__dirname, '/public/contact.html'));
+});
+
+app.listen(3000);

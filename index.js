d42f4f8b24448f480c335bf0c68dd8e3ff046c9b
khushboodhiman <khushboodhiman716@gmail.com>
2022-04-17 23:49:29 +0530

    Question 3

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// QUESTION 2
 // Used because without this node doesn't executes static files like css and pngs
 app.use(express.static(__dirname+'/public'));
 
+// QUESTION 1
 // Route with handler which will be executed when route matches the given route in the statement i.e. / or home
 app.get('/', function(req, res){
 
@@ -15,7 +17,16 @@ app.get('/', function(req, res){
 
 app.get('/about', function(req, res){
     res.sendFile(path.join(__dirname, '/public/about.html'));
- });
+});
 
+// QUESTION 3
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+var clients = ['Google', 'Facebook', 'Ryaz', 'Demo Butler', 'Rana Dev'];
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {data: clients});
+})
 
 app.listen(3000);
